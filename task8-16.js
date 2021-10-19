0001001289d9a52976ace3390b47ac2c064acc3e
01Andrei <mail@gmail.compost1234@tut.by>
2021-10-19 11:27:50 +0300

    init: create tasks8-16
    tasks8-16 performed

---
+++ task8-16.js
@@ -0,0 +1,37 @@
+//Task8
+var sum = 10 + 10;
+
+//Task9
+
+var difference = 45 - 33;
+
+//Task10
+var product = 8 * 10;
+
+//Task11
+var quotient = 66 / 33;
+
+//Task12
+var myVar = 87;
+
+// Only change code below this line
+myVar ++;
+
+
+//Task13
+var myVar = 11;
+
+// Only change code below this line
+myVar --;
+
+
+//Task14
+var ourDecimal = 5.7;
+var myDecimal = 5.7;
+// Only change code below this line
+
+//Task15
+var product = 2.0 * 2.5;
+
+//Task16
+var quotient = 4.4 / 2.0; // Change this line
