3dea9da2c7d045337aed54257bf453aedc511d5a
01Andrei <mail@gmail.compost1234@tut.by>
2021-10-20 12:09:19 +0300

    init: create tasks 17-20
    tasks 17-20 well done

---
+++ task17-20.js
@@ -0,0 +1,47 @@
+//task 17
+
+// Only change code below this line
+
+var remainder = 11 % 3;
+
+
+//Task 18
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+
+//Task 19
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+//Task 19
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *=  5;
+b *= 3 ;
+c *= 10;
+
+//Task 20
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
